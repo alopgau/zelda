5bac52c263a8693dcefc4d0a51e442e4b25b1c55
alopgau <alopezg@ieslabahia.es>
2026-04-11 15:39:29 +0200

    fetch api

---
+++ js/api.js
@@ -0,0 +1,13 @@
+async function obtenerDatos(url) {
+    try {
+        const response = await fetch(url)
+        if (!response.ok) throw new Error(`Error ${response.status}`);
+
+        const data = await response.json();
+        return data
+    } catch (error) {
+        const modal = document.querySelector(".error__modal")
+        modal.classList.toggle("hidden")
+    }
+
+} 
