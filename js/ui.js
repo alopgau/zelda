c0ea8a2c1db4988385ee7e640f4a08af26c7c3ed
alopgau <alopezg@ieslabahia.es>
2026-04-13 21:07:23 +0200

    funcion search api

--- js/ui.js
+++ js/ui.js
@@ -1,13 +1,17 @@
 import { db } from "./firebase.js";
 import { getAPIData } from "./api.js";
-let category = undefined
+
 const readCategory = () => {
-    const categorySelect = document.querySelector("#searchtarget")
-    return categorySelect.value
+    const checks = []
+    const categorySelect = document.querySelectorAll(".option-checker__option")
+    categorySelect.forEach(option => {
+        if (option.checked) checks.push(option.value)
+    })
+    return checks
 }
 const searchBar = document.querySelector("#searchbar")
 searchBar.addEventListener("focus", () => {
-    category = readCategory()
+    const categories = readCategory()
 })
 const debounce = (func, delay) => {
     let timeout;
@@ -20,12 +24,68 @@ const debounce = (func, delay) => {
     }
 }
 searchBar.addEventListener("input", (e) => {
-   const data = debounce(() => searchAPI(category, e), 2000)
-   data.data.forEach(it => {
-   });
+    const data = debounce((e) => searchAPI(categories, e), 2000)
+
 })
-const searchAPI = (category, e) => {
+const searchAPI = (categories, e) => {
     const search = e.target.trim()
-    return getAPIData(category, search)
+    const resultsSection = document.querySelector(".search__results")
+    categories.forEach(category => {
+        const data = getAPIData(category, search)
+        switch (category) {
+            case games:
+                data.data.forEach((game) => {
+                    resultsSection.innerHTML += `<article class="card">
+                <h1 class="card__title">${game.name}</h1>
+                <p class="card__description">${game.description}</p>
+                <time class="game__date">${game.released__date}</time>`
+                })
+                break;
+            case characters:
+                data.data.forEach((character) => {
+                    resultsSection.innerHTML += `<article class="card">
+                    <h1 class="card__title">${character.name}</h1>
+                    <p class="card__description">${character.description}</p>
+                    <p class="character__gender">${character.gender}</p>
+                    <p class="character__race">${character.race}</p>`
+                })
+                break;
+            case dungeons:
+                data.data.forEach((dungeon) => {
+                    resultsSection.innerHTML += `<article class="card">
+                    <h1 class="card__title">${dungeon.name}</h1>
+                    <p class="card__description">${dungeon.description}</p>`
+
+
+                })
+                break;
+            case bosses:
+                data.data.forEach((boss) => {
+                    resultsSection.innerHTML += `<article class="card">
+                    <h1 class="card__title">${boss.name}</h1>
+                    <p class="card__description">${boss.description}</p>`
+
+                })
+                break;
+            case places:
+                data.data.forEach((place) => {
+                    resultsSection.innerHTML += `<article class="card">
+                    <h1 class="card__title">${place.name}</h1>
+                    <p class="card__description">${place.description}</p>`
+
+                })
+            case items:
+                data.data.forEach((item) => {
+                    resultsSection.innerHTML += `<article class="card">
+                    <h1 class="card__title">${item.name}</h1>
+                    <p class="card__description">${item.description}</p>`
+
+                })
+                break
+
+            default:
+                break;
+        }
+    })
 
 }
